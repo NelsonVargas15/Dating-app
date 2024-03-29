e0d463912c64313ea6110ca39d505fafef89c499
calobrito <brito.ar07@gmail.com>
2024-03-28 22:38:43 -0300

    feat: Created Component

---
+++ src/pages/home/Terreneitor.jsx
@@ -0,0 +1,19 @@
+import { Stack  } from "rsuite";
+
+const Terreneitor = () => {
+    return (
+  
+      <Stack justifyContent="center" direction="column" >
+  
+        <h1>If you're reading this, I would like you to know that I'm geniunly proud of you. This is truly a great achievement, and cannot be disregared, if you're reading this, that also means that you're a homosexual XOXO  </h1>
+     
+        <p>Kendrick Lamar:</p>
+        <p>Yeah, I don't really like'im either</p>
+        <p>Quite frankly, I really think he a bitch</p>
+        <p>As a matter of fact, load up the beat, right now</p>
+      </Stack>
+     
+    );
+  };
+  
+  export default Terreneitor;
--- src/routes/MainRouter.jsx
+++ src/routes/MainRouter.jsx
@@ -1,17 +1,17 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import FullLayout from "../components/full/FullLayout";
 import Home from "../pages/home/Home";
-
+import Terreneitor from "../pages/home/Terreneitor";
 
 const MainRoutes = () => {
   return (
     <BrowserRouter>
         <Routes>
-        <Route path="/" element={<FullLayout />}>
-          <Route  path="/home" element={<Home />} />
-          
-        </Route>
-      </Routes>
+          <Route path="/" element={<FullLayout />}>
+              <Route  index element={<Home />} />
+              <Route  path="/terreneitor" element={<Terreneitor />} />
+          </Route>
+        </Routes>
     </BrowserRouter>
   );
 };
